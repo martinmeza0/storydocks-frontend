debdeddd5132264dd5084e3d159ab3f9fdc009b6
Martinmeza0 <martinmezacorreo@gmail.com>
2021-11-26 17:53:20 -0300

    add: creater-react-app

---
+++ src/components/product.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const product = () => {
+    return (
+        <>
+            <figure className="product">
+                <img src="https://images.unsplash.com/photo-1485462537746-965f33f7f6a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" alt="dadad"/>
+                <div>
+                    <h3> black coach</h3>
+                    <p>$10</p>
+                </div>
+
+            </figure>
+        </>
+    )
+}
+
+export default product
---
+++ src/services/products.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const apiUrl = 'http://localhost:3001/api/products'
+
+const getAllproducts = () => {
+    const request = axios.get(apiUrl)
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(apiUrl, newObject)
+    return request.then(response => response.data)
+   
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${apiUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default { getAllproducts, create, update }
